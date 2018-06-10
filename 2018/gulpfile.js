d4d1de613b4b731e77354952ea4c4bbdaee06b83
Kira <leedj2718@gmail.com>
2018-06-10 19:54:04 +0800

    edited gulpfile

--- 2018/gulpfile.js
+++ 2018/gulpfile.js
@@ -97,7 +97,7 @@ gulp.task('watch', function() {
 gulp.task('webserver', function() {
     gulp.src('')
         .pipe(webserver({
-            host: '0.0.0.0',
+            host: '127.0.0.1',
             port: 9527,
             livereload: true,
             directoryListing: false,
