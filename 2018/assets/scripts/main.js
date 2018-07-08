a0747434e7d3c5c261ac9809db0e8a6769a29a31
mosquito chang <bbbb00y@gmail.com>
2018-07-08 21:50:22 +0800

    lightbox

--- 2018/assets/scripts/main.js
+++ 2018/assets/scripts/main.js
@@ -264,7 +264,19 @@ function scrollHandler() {
 }
 
 $("#sign-up").on("click",function(){
-	ga('send', 'event','menu','click', 'menu_signup');
+	$(".lightbox").addClass('active');
+	var tl = new TimelineMax({repeat:0});
+	tl.fromTo($(".lightbox .info"), 0.5, {x:-30,opacity:0},{x:-30,opacity:0});
+	tl.fromTo($(".lightbox .info"), 0.5, {x:-30,opacity:0},{x:0,opacity:1});
+	tl.fromTo($(".lightbox .list:nth-child(2) .title"), 0.5, {x:-30,opacity:0},{x:0,opacity:1});
+	tl.fromTo($(".lightbox .list:nth-child(2) .content"), 0.5, {x:-30,opacity:0},{x:0,opacity:1});
+	tl.fromTo($(".lightbox .list:nth-child(3) .title"), 0.5, {x:-30,opacity:0},{x:0,opacity:1});
+	tl.fromTo($(".lightbox .list:nth-child(3) .content"), 0.5, {x:-30,opacity:0},{x:0,opacity:1});
+})
+
+$(".lightbox .close").on("click",function(){
+	$(".lightbox").removeClass('active');
+	
 })
 
 //steps accordion
